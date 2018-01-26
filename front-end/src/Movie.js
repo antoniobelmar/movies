67a0e19d30f8233e2549c52f7c986c7c0b98448f
antonio <antoniobelmardacosta@gmail.com>
2018-01-26 17:24:50 +0000

    Create movie component to render movie titles, overview, and image

---
+++ front-end/src/Movie.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+
+  render() {
+    return (
+      <div key={this.props.index}>
+        <div className={'movie-title'+this.props.index}> {this.props.movie.title} </div>
+        <div className={'movie-overview'+this.props.index}> {this.props.movie.overview} </div>
+        <img src={'http://image.tmdb.org/t/p/w185/'+this.props.movie.poster_path} alt='Not found'/>
+      </div>
+    )
+  }
+}
+
+
+export default Movie;
